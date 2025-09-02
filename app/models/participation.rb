5ad9e5c0cff79a6c1e68e223b60c40d5813dc736
Nicolas Peltier <nicolas.peltier@gmail.com>
2025-09-02 23:46:52 +0200

    enum corrrect syntax

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -3,7 +3,7 @@ class Participation < ApplicationRecord
   belongs_to :teacher, optional: true # deviendra nil après anonymisation
   has_many :question_responses, dependent: :destroy
   
-  enum status: { not_opened: 0, opened: 1, in_progress: 2, completed: 3 }
+  enum :status, { not_opened: 0, opened: 1, in_progress: 2, completed: 3 }
   
   before_validation :ensure_token
   
